ace39f2f1f0a88721b94331b7f3dd6c8247ec803
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-07-31 16:30:36 -0300

    added next method and testing with Immutable.fromJS

--- test/coreSpec.js
+++ test/coreSpec.js
@@ -1,7 +1,7 @@
-import {List,Map} from 'immutable'
+import {List,Map,fromJS} from 'immutable'
 import {expect} from 'chai'
 
-import {setEntries} from '../src/core'
+import {setEntries,next,vote} from '../src/core'
 
 describe('application logic',()=>{
 
@@ -33,4 +33,71 @@ describe('application logic',()=>{
 
   })
 
+  describe('next',() => {
+
+    it('takes the next two entries under vote',() => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+
+      const nextState = next(state)
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        })
+        ,entries: List.of('Sunshine')
+      }))
+    })
+
+  })
+
+  describe('vote',() => {
+    it('create a tally for the voted entry',() => {
+
+        const state = Map({
+          vote: Map({
+            pair: List.of('Trainspotting','26 Days Later')
+          })
+          ,entries: List()
+        })
+        const nextState = vote(state,'Trainspotting')
+
+        expect(nextState).to.equal(Map({
+          vote: Map({
+            pair: List.of('Trainspotting','26 Days Later')
+            ,tally: Map({
+              'Trainspotting':1
+            })
+          })
+          ,entries: List()
+        }))
+
+    })
+
+    it('add the existing tally for the voted entry',() => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting','26 Days Later')
+          ,tally: Map({
+            'Trainspotting':3
+            ,'26 Days Later':2
+          })
+        })
+        ,entries: List()
+      })
+      const nextState = vote(state,'Trainspotting')
+      expect(nextState).to.equal(fromJS({
+        vote: {
+           pair: ['Trainspotting','26 Days Later']
+          ,tally: {
+            'Trainspotting':4
+            ,'26 Days Later':2
+          }
+        }
+        ,entries: []
+      }))
+
+    })
+  })
 })
