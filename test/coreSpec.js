4f7e2418a9947affa2f5e389fd19caaafe000822
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-01 15:36:43 -0300

    added logic to add winners back to entries list

--- test/coreSpec.js
+++ test/coreSpec.js
@@ -50,6 +50,52 @@ describe('application logic',()=>{
       }))
     })
 
+    it('puts the winner of the current vote back to entries',()=>{
+
+      const state = fromJS({
+        vote:{
+          pair:['Trainspotting', '28 Days Later']
+          ,tally:{
+             'Trainspotting': 4
+            ,'28 Days Later': 2
+          }
+        }
+        ,entries:['Sunshine', 'Millions', '127 Hours']
+      })
+
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS({
+        vote:{
+          pair:['Sunshine', 'Millions']
+        }
+        ,entries:['127 Hours','Trainspotting']
+      }))
+
+    })
+
+    it('puts both tied vote back to entries',()=>{
+
+      const state = fromJS({
+        vote:{
+          pair:['Trainspotting', '28 Days Later']
+          ,tally:{
+             'Trainspotting': 3
+            ,'28 Days Later': 3
+          }
+        }
+        ,entries:['Sunshine', 'Millions', '127 Hours']
+      })
+
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS({
+        vote:{
+          pair:['Sunshine', 'Millions']
+        }
+        ,entries:['127 Hours','Trainspotting','28 Days Later']
+      }))
+    })
   })
 
   describe('vote',() => {
