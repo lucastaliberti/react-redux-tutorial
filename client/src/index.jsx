55beee5784a35de98d7a9144bcb8c130cbb98643
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-03 14:54:11 -0300

    added react router and App component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
+import App from './components/App.jsx'
 import Voting from './components/Voting.jsx'
 
-const pair = ['Trainspottin','26 Days Later']
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>
 
 ReactDom.render(
-  <Voting pair={pair} />
+  <Router history={hashHistory}>{routes}</Router>
+  //<Voting pair={['Trainspotting','26 Days Later']} />
   ,document.getElementById('app')
 )
