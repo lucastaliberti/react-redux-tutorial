9505bbfcdf33a4664b7a0c40b4502a3fa469b1bb
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-04 10:43:07 -0300

    refactor Results component and add tests for it

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import {List} from 'immutable'
+import {List,Map} from 'immutable'
 
 const pair = List.of('Trainspotting','26 Days Later')
+const tally = Map({'Trainspotting': 5,'26 Days Later': 4})
 
 export default class App extends React.PureComponent
 {
   render(){
-    return React.cloneElement(this.props.children, {pair:pair})
+    return React.cloneElement(this.props.children, {
+       pair: pair
+      ,tally: tally
+    })
   }
 }
--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 export default class Results extends React.PureComponent
 {
+  getPair(){
+    return this.props.pair || []
+  }
+  getVotes(entry){
+    if(this.props.tally && this.props.tally.has(entry))
+      return this.props.tally.get(entry)
+
+    return 0
+  }
   render(){
-    return <div>Hello from results!</div>
+    return <div className='results'>
+      {this.getPair().map(entry =>
+        <div key={entry} className='entry'>
+          <h1>{entry}</h1>
+          <div className='voteCount'>
+            {this.getVotes(entry)}
+          </div>
+        </div>
+      )}
+    </div>
   }
 }
