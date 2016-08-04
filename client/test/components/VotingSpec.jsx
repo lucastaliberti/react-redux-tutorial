beb45cbea45049a53e992f209d79b88c4d79e554
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-04 15:44:34 -0300

    added onclick on component and test for vote

--- client/test/components/VotingSpec.jsx
+++ client/test/components/VotingSpec.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {
   renderIntoDocument
   ,scryRenderedDOMComponentsWithTag
+  ,Simulate
 } from 'react-addons-test-utils'
 import {List} from 'immutable'
 import {expect} from 'chai'
@@ -20,6 +21,21 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2)
     expect(buttons[0].textContent).to.equal('Trainspotting')
     expect(buttons[1].textContent).to.equal('26 Days Later')
+  })
+
+  it('invokes a callback when a button is cliked', () => {
+    let votedWith
+    const vote = (entry) => votedWith = entry
+    const pair = List.of('Trainspotting','26 Days Later')
+
+    const component = renderIntoDocument(
+      <Voting pair={pair} vote={vote} />
+    )
+
+    const buttons = scryRenderedDOMComponentsWithTag(component,'button')
+    Simulate.click(buttons[0])
+
+    expect(votedWith).to.equal('Trainspotting')
   })
 
   it('adds a label to voted entry', () => {
@@ -90,4 +106,6 @@ describe('Voting', () => {
     firstButton = scryRenderedDOMComponentsWithTag(component,'button')[0]
     expect(firstButton.textContent).to.equal('Sunshine')
   })
+
+
 })
