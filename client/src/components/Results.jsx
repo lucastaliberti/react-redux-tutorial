baaa86c478a5078166fe84e437ff4eb1711b55c6
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-04 10:26:30 -0300

    added Results component and route

---
+++ client/src/components/Results.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default class Results extends React.PureComponent
+{
+  render(){
+    return <div>Hello from results!</div>
+  }
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,13 +3,14 @@ import ReactDom from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 import App from './components/App.jsx'
 import Voting from './components/Voting.jsx'
+import Results from './components/Results.jsx'
 
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>
 
 ReactDom.render(
   <Router history={hashHistory}>{routes}</Router>
-  //<Voting pair={['Trainspotting','26 Days Later']} />
   ,document.getElementById('app')
 )
