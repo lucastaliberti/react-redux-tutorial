00fc901eb179551569d350b5536fbc9ca2b1a8eb
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-09 16:03:22 -0300

    added hasVoted check

--- client/src/components/Voting.jsx
+++ client/src/components/Voting.jsx
@@ -5,14 +5,25 @@ import Winner from './Winner.jsx'
 export default class Voting extends React.PureComponent
 {
   getPair() {
-      return this.props.pair || []
+    return this.props.pair || []
+  }
+  isDisabled(){
+    return !!this.props.hasVoted
+  }
+  hasVotedFor(entry){
+    return this.props.hasVoted === entry
   }
   render() {
     return <div className="voting">
       {this.getPair().map(entry =>
         <button key={entry}
+                disabled={this.isDisabled()}
                 onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ?
+            <div className="label">Voted</div> :
+            null
+          }
         </button>
       )}
     </div>
